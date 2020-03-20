69e66340ee9326593d4226c4684d54b9d242659c
Amantina Sosa <amantinasosa@gmail.com>
2020-03-20 14:04:32 -0400

    added youtube api

--- server/api/index.js
+++ server/api/index.js
@@ -4,6 +4,7 @@ module.exports = router
 router.use('/users', require('./users'))
 router.use('/images', require('./image'))
 router.use('/videos', require('./video'))
+router.use('/youtube', require('./youtube'))
 
 router.use((req, res, next) => {
   const error = new Error('Not Found')
---
+++ server/api/youtube.js
@@ -0,0 +1,26 @@
+const router = require('express').Router()
+const axios = require('axios')
+module.exports = router
+
+//num of videos you can recieve
+const max = 2
+const API = process.env.YOUTUBE_CLIENT_SECRET || require('secrets.js')
+
+router.get('/', async (req, res, next) => {
+  try {
+    // console.log('here')
+    //q is used to input what youre querying for
+    //asl videos
+    const q = 'abc'
+    const channelId = 'UC7fVfWv6FL7HeTFeSLz-muQ'
+    //find playlist inspect page source and add the id here & then add it into the url after channel id with &${playlistId}
+    const playlistId = ''
+    //search param searching youtube ?
+    const url = `https://www.googleapis.com/youtube/v3/search?part=snippet&${channelId}&maxResults=${max}&q=${q}&type=video&videoEmbeddable=true&key=${API}`
+
+    const {data} = await axios.get(url)
+    res.send(data)
+  } catch (error) {
+    next(error)
+  }
+})
